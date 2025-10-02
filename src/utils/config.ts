48723a7992c918f8988d544c7c8c128ef3d24022
Carlos Grillo <cadugrillo@gmail.com>
2025-10-02 14:51:57 -0400

    added option for IEM conf

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 interface Config {
 	URL: string;
 	userName: string;
@@ -6,10 +8,29 @@ interface Config {
 	transport: string;
 }
 
+const urlCheck = () => {
+
+	const pathToConfig = 'home/edge/s7-mcp-server/configs/config.json';
+	if (fs.existsSync(pathToConfig)) {
+		fs.readFile(pathToConfig, 'utf8', (err, data) => {
+			if (err) {
+				console.error('Error reading JSON file:', err);
+				return process.env["PLC_API_URL"];
+			}
+			const config = JSON.parse(data);
+			return config.plcApiUrl || process.env["PLC_API_URL"];
+		});
+	}
+
+	return process.env["PLC_API_URL"];
+}
+
 export const config: Config = {
-	URL: process.env["PLC_API_URL"] || "",
+	URL: urlCheck() || "",
 	userName: process.env["PLC_USER_NAME"] || "",
 	pwd: process.env["PLC_USER_PASSWD"] || "",
 	mcpServerPort: process.env["MCP_SERVER_PORT"] || 5000,
 	transport: process.env["TRANSPORT"] || "streamable-http"
 };
+
+
